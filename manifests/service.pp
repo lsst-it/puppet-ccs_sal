874791094143a892e0d5dc71ea45af8b6aec17b8
Glenn Morris <rgm@stanford.edu>
2021-04-09 12:39:59 -0700

    Add -dev versions of some services, disabled by default

--- manifests/service.pp
+++ manifests/service.pp
@@ -21,27 +21,52 @@ class ccs_sal::service {
     }
   }
 
+
+  $ocs_vars = {
+    desc  => "CCS OCS bridge for ${instrument}",
+    env   => 'LSST_DDS_HISTORYSYNC=0',
+    start => "/opt/lsst/ccs/prod/bin/ocs-bridge-${instrument}",
+  }
+
   $ocs_bridge = {
     service  => "ocs-bridge-${instrument}",
     template => 'ocs-bridge',
-    vars     => {
-      desc  => "CCS OCS bridge for ${instrument}",
-      env   => 'LSST_DDS_HISTORYSYNC=0',
-      start => "/opt/lsst/ccs/prod/bin/ocs-bridge-${instrument}",
+    vars     => $ocs_vars,
+  }
+
+  $ocs_bridge_dev = $ocs_bridge + {
+    service => "${ocs_bridge['service']}-dev",
+    vars    => $ocs_vars + {
+      desc      => "${ocs_vars['desc']} (dev version)",
+      start     => regsubst( $ocs_vars['start'], '\/prod\/', '/dev/'),
+      conflicts => "${ocs_bridge['service']}.service",
     }
+  }
+
+
+  $mcm_vars = {
+    desc  => "CCS MCM for ${instrument}",
+    env   => '',
+    start => "/opt/lsst/ccs/prod/bin/mcm-${instrument}",
   }
 
   $mcm = {
     service  => "mcm-${instrument}",
     template => 'mcm',
-    vars     => {
-      desc    => "CCS MCM for ${instrument}",
-      env     => '',
-      start   => "/opt/lsst/ccs/prod/bin/mcm-${instrument}",
+    vars     => $mcm_vars
+  }
+
+  $mcm_dev = $mcm + {
+    service => "${mcm['service']}-dev",
+    vars    => $mcm_vars + {
+      desc      => "${mcm_vars['desc']} (dev version)",
+      start     => regsubst( $mcm_vars['start'], '\/prod\/', '/dev/'),
+      conflicts => "${mcm['service']}.service",
     }
   }
 
-  $services = [$opensplice, $ocs_bridge, $mcm]
+
+  $services = [$opensplice, $ocs_bridge, $mcm, $ocs_bridge_dev, $mcm_dev]
 
   $services.each | $hash | {
     $service  = $hash['service']
@@ -51,7 +76,7 @@ class ccs_sal::service {
       content => epp("${module_name}/${template}.service.epp", $epp_vars),
     }
     -> service { $service:
-      enable => true,
+      enable => $service !~ /-dev$/,
     }
   }
 
