48927978935b336d2e70a95b3b852ebc77257466
Joshua Hoblitt <josh@hoblitt.com>
2022-08-15 15:52:59 -0700

    add basic acceptance spec

--- spec/spec_helper_acceptance.rb
+++ spec/spec_helper_acceptance.rb
@@ -5,6 +5,9 @@
 
 require 'voxpupuli/acceptance/spec_helper_acceptance'
 
-configure_beaker
+configure_beaker do |host|
+  # /etc/ccs is owned by lsst/ccs_software
+  on host, 'mkdir -p /etc/ccs'
+end
 
 Dir['./spec/support/acceptance/**/*.rb'].sort.each { |f| require f }
