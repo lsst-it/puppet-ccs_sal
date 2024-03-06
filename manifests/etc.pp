ae379f953c823800aacd9655194a44e8ecd95fea
Glenn Morris <rgm@stanford.edu>
2024-03-06 13:33:48 -0800

    Add option to use kafka instead of dds
    New class parameters:
    (dds, kafka) boolean switches
    (kafka_broker_address, kafka_registry_url, kafka_sasl_password)
    (kafka_sasl_username, kafka_templates_directory) strings with kafka values
    
    (rpms) nothing to install when not using dds.
    (services) no opensplice when not using dds.
    (etc) add kafka setup file when relevant, skip dds when not.
    Add kafka line to .app files when relevant. Use a template for app files.
    Always use setup-sal as the pre-execute file.
    
    (files/setup-sal) new file
    (templates/ocs-app.epp, templates/setup-sal-kafka.epp) new templates

--- manifests/etc.pp
+++ manifests/etc.pp
@@ -2,6 +2,9 @@
 #   Configure /etc for CCS/SAL
 #
 class ccs_sal::etc {
+  $dds = $ccs_sal::dds
+  $kafka = $ccs_sal::kafka
+
   $dir = '/etc/ccs'
 
   $attributes = {
@@ -13,24 +16,55 @@ class ccs_sal::etc {
   $ptitle = regsubst($title, '::.*', '', 'G')
 
   $salfile = 'setup-sal5'
+  $kafka_salfile = 'setup-sal-kafka'
+  $prefile = 'setup-sal'
 
-  file { "${dir}/${salfile}":
-    ensure  => file,
-    content => epp("${ptitle}/${salfile}.epp", {
-        'domain'    => $ccs_sal::dds_domain,
-        'interface' => $ccs_sal::dds_interface,
-        'home'      => $ccs_sal::ospl_home,
-      },
-    ),
-    *       => $attributes,
+  file { "${dir}/${prefile}":
+    ensure => file,
+    source => "puppet:///modules/${ptitle}/${prefile}",
+    *      => $attributes,
   }
 
-  ['ospl-shmem.xml', 'QoS.xml'].each|$thing| {
-    $file = "${dir}/${thing}"
-    file { $file:
-      ensure => file,
-      source => "puppet:///modules/${ptitle}/${thing}",
-      *      => $attributes,
+  if $kafka {
+    $implementation = 'system.property.org.lsst.sal.implementation=kafka'
+  } else {
+    $implementation = undef
+  }
+
+  if $kafka {
+    file { "${dir}/${kafka_salfile}":
+      ensure  => file,
+      content => epp("${ptitle}/${kafka_salfile}.epp", {
+          'templates_directory' => $ccs_sal::kafka_templates_directory,
+          'broker_address'      => $ccs_sal::kafka_broker_address,
+          'sasl_username'       => $ccs_sal::kafka_sasl_username,
+          'sasl_password'       => $ccs_sal::kafka_sasl_password,
+          'registry_url'        => $ccs_sal::kafka_registry_url,
+        },
+      ),
+      *       => $attributes,
+    }
+  }
+
+  if $dds {
+    file { "${dir}/${salfile}":
+      ensure  => file,
+      content => epp("${ptitle}/${salfile}.epp", {
+          'domain'    => $ccs_sal::dds_domain,
+          'interface' => $ccs_sal::dds_interface,
+          'home'      => $ccs_sal::ospl_home,
+        },
+      ),
+      *       => $attributes,
+    }
+
+    ['ospl-shmem.xml', 'QoS.xml'].each|$thing| {
+      $file = "${dir}/${thing}"
+      file { $file:
+        ensure => file,
+        source => "puppet:///modules/${ptitle}/${thing}",
+        *      => $attributes,
+      }
     }
   }
 
@@ -39,20 +73,26 @@ class ccs_sal::etc {
     $file = "${dir}/${instrument}-ocs-${thing}.app"
     file { $file:
       ensure  => file,
-      content => "system.pre-execute=${salfile}\n",
+      content => epp("${ptitle}/ocs-app.epp", {
+          'prefile' => $prefile,
+          'extra'   => $implementation,
+        },
+      ),
       *       => $attributes,
     }
   }
 # lint:endignore
 
-  ## Stop tmpfiles.d removing opensplice sockets.
-  $tmpfiles_conf = 'ccs-ospl.conf'
+  if $dds {
+    ## Stop tmpfiles.d removing opensplice sockets.
+    $tmpfiles_conf = 'ccs-ospl.conf'
 
-  file { "/etc/tmpfiles.d/${tmpfiles_conf}":
-    ensure => file,
-    source => "puppet:///modules/${ptitle}/${tmpfiles_conf}",
-    owner  => 'root',
-    group  => 'root',
-    mode   => '0644',
+    file { "/etc/tmpfiles.d/${tmpfiles_conf}":
+      ensure => file,
+      source => "puppet:///modules/${ptitle}/${tmpfiles_conf}",
+      owner  => 'root',
+      group  => 'root',
+      mode   => '0644',
+    }
   }
 }
