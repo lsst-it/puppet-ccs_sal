af622196e8a23bfe65b3da3063bbb6dd68d5a56d
Glenn Morris <rgm@stanford.edu>
2026-04-25 18:44:12 -0700

    fix example
    The ts_yum repo doesn't retain old package versions.
    It seems that ts_sal_utils no longer exists, but ts_sal_utilsKafka does.

--- examples/basic.pp
+++ examples/basic.pp
@@ -13,6 +13,6 @@ group { 'ccsadm':
 
 class { 'ccs_sal':
   rpms => {
-    ts_sal_utils => 'ts_sal_utils-7.4.0-1.x86_64.rpm',
+    ts_sal_utilsKafka => 'ts_sal_utilsKafka-10.1.1-1.x86_64.rpm',
   },
 }
